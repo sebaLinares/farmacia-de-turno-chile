d885f69a88993625a542ce3b46a9006629cf6e31
Sebastian Linares <slinaresl@gmail.com>
2019-07-26 19:39:46 -0400

    refactor: change regiones to .js to export it

---
+++ src/Database/regiones.js
@@ -0,0 +1,24 @@
+export const regiones = [
+  { nombre: 'ARICA Y PARINACOTA', fk_region: 1 },
+  { nombre: 'REGION DE TARAPACA', fk_region: 2 },
+  { nombre: 'REGION DE ANTOFAGASTA', fk_region: 3 },
+  { nombre: 'REGION DE ATACAMA', fk_region: 4 },
+  { nombre: 'REGION DE COQUIMBO', fk_region: 5 },
+  { nombre: 'REGION DE VALPARAISO', fk_region: 6 },
+  { nombre: 'REGION METROPOLITANA', fk_region: 7 },
+  {
+    nombre: 'REGION DEL LIBERTADOR GENERAL BERNADO OHIGGINS',
+    fk_region: 8,
+  },
+  { nombre: 'REGION DEL MAULE', fk_region: 9 },
+  { nombre: 'REGION DEL BIO-BIO', fk_region: 10 },
+  { nombre: 'REGION DE LA ARAUCANIA', fk_region: 11 },
+  { nombre: 'REGION DE LOS RIOS', fk_region: 12 },
+  { nombre: 'REGION DE LOS LAGOS', fk_region: 13 },
+  {
+    nombre: 'REGION DE AYSEN DEL GENERAL CARLOS IBAÑEZ DEL CAMPO',
+    fk_region: 14,
+  },
+  { nombre: 'REGION DE MAGALLANES Y LA ANTARTICA CHILENA', fk_region: 15 },
+  { nombre: 'REGION DE ÑUBLE', fk_region: 16 },
+];
