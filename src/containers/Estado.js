acd8ab942fe63da17f5b2d1b4b41495e71b668e8
Sebastian Linares <slinaresl@gmail.com>
2019-07-27 14:30:36 -0400

    refactor: change to functional comp & react hooks

--- src/containers/Estado.js
+++ src/containers/Estado.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './Estado.module.scss';
 
-class Estado extends Component {
-  state = {
+const Estado = () => {
+  const [estado, setEstado] = useState({
     version: 'V.0.2',
     ultimaActualizacion: '26-04-2019',
-  };
-  render() {
-    return (
-      <div className={styles.Estado}>
-        <div className={styles.box}>
-          Version: <span>{this.state.version}</span>
-        </div>
-        <div className={styles.box}>
-          <p>Ultima actualización: </p>
-          <span>{this.state.ultimaActualizacion}</span>
-        </div>
-        <div className={styles.box}>
-          <p>Repositorio </p>
-          <a href="https://github.com/sebaLinares/farmacia-de-turno-chile-react">Github</a>
-        </div>
+  });
+
+  return (
+    <div className={styles.Estado}>
+      <div className={styles.box}>
+        Version:
+        {' '}
+        <span>{estado.version}</span>
       </div>
-    );
-  }
-}
+      <div className={styles.box}>
+        <p>Ultima actualización: </p>
+        <span>{estado.ultimaActualizacion}</span>
+      </div>
+      <div className={styles.box}>
+        <p>Repositorio </p>
+        <a href="https://github.com/sebaLinares/farmacia-de-turno-chile-react">Github</a>
+      </div>
+    </div>
+  );
+};
 
 export default Estado;
