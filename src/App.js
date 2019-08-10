e9813d7beffa3e9fda4594693b0ab0e4579f25c3
Sebastian Linares <slinaresl@gmail.com>
2019-08-10 19:39:49 -0400

    feat: start adding styles components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 import React, { useState } from 'react';
-import styles from './App.module.scss';
+import styled, { ThemeProvider } from 'styled-components';
+// import styles from './App.module.scss';
 import Farmacias from './containers/Farmacias';
 import Header from './containers/Header';
 import Estado from './containers/Estado';
-import GoogleSignIn from './containers/GoogleSignIn';
+import GoogleAuth from './containers/GoogleAuth';
+import theme from './theme/theme';
 
 const App = () => {
   const [comunaSelected, setComunaSelected] = useState('');
   const headerHandler = (dataFromHeader) => {
     setComunaSelected(dataFromHeader);
   };
+  const AppDiv = styled.div`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    // padding: 50px 20px;
+    width: auto;
+    box-sizing: border-box;
+    justify-content: space-between;
+  `;
 
-  const signOut = () => {
-    window.gapi.auth2
-      .init({
-        client_id: '73756367210-pps5o33esgvgk5e7c13sf8m8v9e92dvp.apps.googleusercontent.com',
-      })
-      .then(res => res.signOut())
-      .catch(err => console.error(err));
-  };
+  const EstadoContainer = styled.div`
+    min-width: 300px;
+    height: 250px;
+  `;
+
+  const FarmaciasContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-radius: 16px;
+    overflow: hidden;
+    box-sizing: border-box;
+    background-color: #fff;
+    flex-grow: 1;
+    padding-bottom: 50px;
+  `;
+
   return (
-    <div className={styles.App}>
-      <GoogleSignIn />
-      <button type="button" onClick={signOut}>
-        Sign out
-      </button>
-      <div className={styles.estadoContainer}>
+    <AppDiv>
+      <ThemeProvider theme={theme}>
+        <GoogleAuth />
+      </ThemeProvider>
+      <EstadoContainer>
         <Estado />
-      </div>
-      <div className={styles.farmaciasContainer}>
+      </EstadoContainer>
+      <FarmaciasContainer>
         <Header comunaHandler={headerHandler} />
         <Farmacias comunaSelected={comunaSelected} />
-      </div>
-    </div>
+      </FarmaciasContainer>
+    </AppDiv>
   );
 };
 
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,15 +1,15 @@
-const color = 'blue';
-const theme_text = '#cccccc';
-const label_theme = '#2d5a6b';
-const label_text = theme_text;
-const label_border = `1px solid ${theme_text}`;
+const gray = 'hsla(0, 0%, 0%, 1)';
+const black = 'hsla(208, 97%, 15%, 1)';
+const green = 'hsla(180, 77%, 45%, 1)';
+const darkGreen = 'hsla(191, 80%, 25%, 1)';
+const lightBlue = 'hsla(186%, 98%, 95%, 1)';
 
 const theme = {
-  color, // I don't need color: color here because the names are the same
-  'theme-text': theme_text,
-  'label-theme': label_theme,
-  'label-text': label_text,
-  'label-border': label_border,
+  gray,
+  black,
+  green,
+  darkGreen,
+  lightBlue,
   font:
     '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
 };
