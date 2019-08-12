6676920126ae2d855799fbe2c8ef42a6005b8dc6
Sebastian Linares <slinaresl@gmail.com>
2019-08-11 22:56:58 -0400

    refactor: create light and dark theme objects

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,4 +1,5 @@
-const gray = 'hsla(0, 0%, 0%, 1)';
+const white = 'hsla(186, 0%, 100%, 1)';
+const gray = 'hsla(0, 0%, 95%, 1)';
 const black = 'hsla(208, 97%, 15%, 1)';
 const green = 'hsla(180, 77%, 45%, 1)';
 const darkGreen = 'hsla(191, 80%, 25%, 1)';
@@ -6,6 +7,7 @@ const lightBlue = 'hsla(186%, 98%, 95%, 1)';
 
 const theme = {
   gray,
+  white,
   black,
   green,
   darkGreen,
