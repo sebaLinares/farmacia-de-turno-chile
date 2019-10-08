65993aa0638fa043a27a751bee4f2f830518acbf
Sebastian Linares <slinaresl@gmail.com>
2019-10-07 22:38:17 -0300

    refactor: first theme implementation

--- src/App.js
+++ src/App.js
@@ -1,57 +1,54 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-// import styles from './App.module.scss';
-import Farmacias from './containers/Farmacias';
-import Header from './containers/Header';
-import Estado from './containers/Estado';
-import GoogleAuth from './containers/GoogleAuth';
-import theme from './theme/theme';
+import { mainBackgroundImage, mainBackgroundColour } from './theme/theme';
 
-const App = () => {
-  const [comunaSelected, setComunaSelected] = useState('');
-  const headerHandler = (dataFromHeader) => {
-    setComunaSelected(dataFromHeader);
+class App extends Component {
+  PROFILE_URL = './assets/foto-perfil-linkedin.png';
+
+  state = {
+    theme: 'light',
   };
-  const AppDiv = styled.div`
-    position: relative;
-    display: flex;
-    flex-wrap: wrap;
-    // padding: 50px 20px;
-    width: auto;
-    box-sizing: border-box;
-    justify-content: space-between;
-  `;
 
-  const EstadoContainer = styled.div`
-    min-width: 300px;
-    height: 250px;
-  `;
+  changeTheme = () => {
+    const { theme } = this.state;
+    if (theme === 'light') {
+      this.setState({ theme: 'dark' });
+      return;
+    }
+    this.setState({ theme: 'light' });
+  };
 
-  const FarmaciasContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    border-radius: 16px;
-    overflow: hidden;
-    box-sizing: border-box;
-    background-color: #fff;
-    flex-grow: 1;
-    padding-bottom: 50px;
-  `;
+  render() {
+    const AppDiv = styled.div`
+      height: 100vh;
+      width: 100vw;
+      background: ${mainBackgroundImage};
+      position: relative;
+      &:before {
+        z-index: -1;
+        position: absolute;
+        content: '';
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: ${mainBackgroundColour};
+      }
+    `;
 
-  return (
-    <AppDiv>
-      <ThemeProvider theme={theme}>
-        <GoogleAuth />
+    const { theme } = this.state;
+
+    return (
+      <ThemeProvider theme={{ mode: theme }}>
+        <AppDiv>
+          <h1>HI</h1>
+          <button type="button" onClick={this.changeTheme}>
+            Change me!
+          </button>
+        </AppDiv>
       </ThemeProvider>
-      <EstadoContainer>
-        <Estado />
-      </EstadoContainer>
-      <FarmaciasContainer>
-        <Header comunaHandler={headerHandler} />
-        <Farmacias comunaSelected={comunaSelected} />
-      </FarmaciasContainer>
-    </AppDiv>
-  );
-};
+    );
+  }
+}
 
 export default App;
