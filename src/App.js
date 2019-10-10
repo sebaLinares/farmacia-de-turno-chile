62de8695401380c39ed60d24dbc4df0d4f307800
sebaLinares <slinaresl@gmail.com>
2019-10-10 14:49:01 -0300

    refactor: change to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 // import styles from './App.module.scss';
 import Farmacias from './containers/Farmacias';
@@ -7,51 +7,30 @@ import Estado from './containers/Estado';
 import GoogleAuth from './containers/GoogleAuth';
 import theme from './theme/theme';
 
-const App = () => {
-  const [comunaSelected, setComunaSelected] = useState('');
-  const headerHandler = (dataFromHeader) => {
+class App extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      comunaSelected: '',
+    };
+  }
+
+  headerHandler = (dataFromHeader) => {
     setComunaSelected(dataFromHeader);
   };
-  const AppDiv = styled.div`
-    position: relative;
-    display: flex;
-    flex-wrap: wrap;
-    // padding: 50px 20px;
-    width: auto;
-    box-sizing: border-box;
-    justify-content: space-between;
-  `;
 
-  const EstadoContainer = styled.div`
-    min-width: 300px;
-    height: 250px;
-  `;
-
-  const FarmaciasContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    border-radius: 16px;
-    overflow: hidden;
-    box-sizing: border-box;
-    background-color: #fff;
-    flex-grow: 1;
-    padding-bottom: 50px;
-  `;
-
-  return (
-    <AppDiv>
-      <ThemeProvider theme={theme}>
-        <GoogleAuth />
-      </ThemeProvider>
-      <EstadoContainer>
-        <Estado />
-      </EstadoContainer>
-      <FarmaciasContainer>
-        <Header comunaHandler={headerHandler} />
-        <Farmacias comunaSelected={comunaSelected} />
-      </FarmaciasContainer>
-    </AppDiv>
-  );
-};
+  render() {
+    const AppDiv = styled.div`
+      position: relative;
+      display: flex;
+      flex-wrap: wrap;
+      // padding: 50px 20px;
+      width: auto;
+      box-sizing: border-box;
+      justify-content: space-between;
+    `;
+    return <AppDiv />;
+  }
+}
 
 export default App;
