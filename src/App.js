c5a680a2d4a035d97040c797e13b7dd5f3d3d314
Sebastian Linares <slinaresl@gmail.com>
2019-10-12 20:34:00 -0300

    refactor: change to functional comp

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React, { Component } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { mainBackgroundImage, mainBackgroundColour } from './theme/theme';
+import React from 'react';
+import styled from 'styled-components';
 
-class App extends Component {
-  constructor() {
-    super(props);
-    this.state = {
-      comunaSelected: '',
-    };
-  }
-
-  headerHandler = (dataFromHeader) => {
-    setComunaSelected(dataFromHeader);
-  };
-
-  render() {
-    const AppDiv = styled.div`
-      position: relative;
-      display: flex;
-      flex-wrap: wrap;
-      // padding: 50px 20px;
-      width: auto;
-      box-sizing: border-box;
-      justify-content: space-between;
-    `;
-    return <AppDiv />;
-  }
-}
+const App = () => {
+  const AppDiv = styled.div`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    // padding: 50px 20px;
+    width: auto;
+    box-sizing: border-box;
+    justify-content: space-between;
+  `;
+  return <AppDiv />;
+};
 
 export default App;
