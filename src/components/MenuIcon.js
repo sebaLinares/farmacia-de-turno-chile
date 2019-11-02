0fed21b878a90b24a75c97982f07244e51440210
Sebastian Linares <slinaresl@gmail.com>
2019-11-02 03:13:56 -0300

    refactor: new shape of menu icon

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,60 +1,19 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import { menuBtnColor } from '../theme/theme';
+import StyledMenuButton from '../StyledComps/StyledMenuButton';
+import StyledButtonBar from '../StyledComps/StyledButtonBar';
 
-const ButtonBar = styled.div`
-  height: 4px;
-  margin: 5px auto;
-  background-color: ${menuBtnColor};
-  transition: all 0.3s ease-out;
-`;
+const MenuIcon = ({ isOpen, toggleMenu }) => (
+  <StyledMenuButton isOpen={isOpen} onClick={toggleMenu}>
+    <StyledButtonBar />
+    <StyledButtonBar />
+    <StyledButtonBar />
+  </StyledMenuButton>
+);
 
-const MenuButton = styled.div`
-  position: absolute;
-  top: 5%;
-  right: 5%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  ${ButtonBar}:nth-child(1) {
-    width: 42px;
-    transform: ${props => props.isOpen
-      && css`
-        rotate(45deg) translate(8px, 12px);
-    `};
-  }
-  ${ButtonBar}:nth-child(2) {
-    width: 32px;
-    ${props => props.isOpen
-      && css`
-        transform: rotate(-45deg) translate(-12px, -1.5px);
-        width: 20px;
-      `};
-  }
-  ${ButtonBar}:nth-child(3) {
-    width: 20px;
-    transform: ${props => props.isOpen
-      && css`
-        rotate(-45deg) translate(19px, -11px);
-      `};
-  }
-`;
-
-const MenuIcon = ({ isOpen, toggleMenu }) => {
-  MenuIcon.propTypes = {
-    toggleMenu: PropTypes.func.isRequired,
-    isOpen: PropTypes.bool.isRequired,
-  };
-
-  return (
-    <MenuButton isOpen={isOpen} onClick={toggleMenu}>
-      <ButtonBar />
-      <ButtonBar />
-      <ButtonBar />
-    </MenuButton>
-  );
+MenuIcon.propTypes = {
+  toggleMenu: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
 };
 
 export default MenuIcon;
