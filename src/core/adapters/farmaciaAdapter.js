9399dc0aa0e85c9ca02b00eb0a0cb242dc6b25e3
Sebastian Linares <slinaresl@gmail.com>
2019-11-02 03:06:06 -0300

    refactor: add more properties to entity farmacia

--- src/core/adapters/farmaciaAdapter.js
+++ src/core/adapters/farmaciaAdapter.js
@@ -3,12 +3,68 @@ import Farmacia from '../entities/farmacia';
 
 export default class {
   farmaciasToView(farmacias) {
+    function farmaciasSegunRegion(numeroRegion) {
+      if (numeroRegion === '1') {
+        return 'Region de Arica y Parinatoca';
+      }
+      if (numeroRegion === '2') {
+        return 'Region de Tarapacá';
+      }
+      if (numeroRegion === '3') {
+        return 'Region de Antofagasta';
+      }
+      if (numeroRegion === '4') {
+        return 'Region de Atacama';
+      }
+      if (numeroRegion === '5') {
+        return 'Region de Coquimbo';
+      }
+      if (numeroRegion === '6') {
+        return 'Region de Valparaiso';
+      }
+      if (numeroRegion === '7') {
+        return 'Región Metropolitana';
+      }
+      if (numeroRegion === '8') {
+        return 'Región del Libertador Bernardo Ohiggins';
+      }
+      if (numeroRegion === '9') {
+        return 'Región del Maule';
+      }
+      if (numeroRegion === '10') {
+        return 'Región del Bío-Bío';
+      }
+      if (numeroRegion === '11') {
+        return 'Región de la Araucanía';
+      }
+      if (numeroRegion === '12') {
+        return 'Región de Los Rios';
+      }
+      if (numeroRegion === '13') {
+        return 'Región de Los Lagos';
+      }
+      if (numeroRegion === '14') {
+        return 'Región de Aysen del General Carlos Ibáñez del Capo';
+      }
+      if (numeroRegion === '15') {
+        return 'Región de Magallanes y la Antártica Chilena';
+      }
+      if (numeroRegion === '16') {
+        return 'Región de Ñuble';
+      }
+      return null;
+    }
+
     const formatedFarmacias = farmacias.map(
       farmacia => new Farmacia({
+        idFarmacia: farmacia.local_id,
         nombreFarmacia: farmacia.local_nombre,
         comuna: farmacia.comuna_nombre,
+        idComuna: farmacia.fk_comuna,
         localidad: farmacia.localidad_nombre,
         direccion: farmacia.local_direccion,
+        region: farmaciasSegunRegion(farmacia.fk_region),
+        idRegion: farmacia.fk_region,
       }),
     );
 
--- src/core/entities/farmacia.js
+++ src/core/entities/farmacia.js
@@ -1,13 +1,21 @@
 export default class {
   constructor({
+    idFarmacia,
     nombreFarmacia, //
     comuna,
+    idComuna,
     localidad,
     direccion,
+    region,
+    idRegion,
   }) {
+    this.idFarmacia = idFarmacia;
     this.nombreFarmacia = nombreFarmacia;
     this.comuna = comuna;
+    this.idComuna = idComuna;
     this.localidad = localidad;
     this.direccion = direccion;
+    this.region = region;
+    this.idRegion = idRegion;
   }
 }
