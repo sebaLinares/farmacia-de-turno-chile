8ea8afd899dd6d1721f34024416a0aa02290c5cc
Sebastian Linares <slinaresl@gmail.com>
2019-11-02 03:09:34 -0300

    feat: inicio component working

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import StyledMainCard from '../StyledComps/StyledMainCard';
+import StyledText from '../StyledComps/StyledText';
+import StyledButton from '../StyledComps/StyledButton';
 
-const Inicio = () => (
-  <StyledMainCard>
-    <p>hi</p>
-  </StyledMainCard>
-);
+const Inicio = ({ history }) => {
+  const goToRegiones = () => {
+    history.push({ pathname: '/regiones' });
+  };
+  return (
+    <StyledMainCard>
+      <StyledText>Bienvenido al buscador de farmacias de turno</StyledText>
+      <StyledText>
+        Haz click en el botón
+        {' '}
+        <span role="img" aria-label="point-down-emoji">
+          👇
+        </span>
+        {' '}
+        y busca la
+      </StyledText>
+      <StyledText>
+        farmacia más cercana a tu
+        {' '}
+        <span role="img" aria-label="house">
+          🏠
+        </span>
+        !
+      </StyledText>
+      <br />
+      <br />
+      <StyledButton type="button" onClick={goToRegiones}>
+        <p>comenzar</p>
+      </StyledButton>
+    </StyledMainCard>
+  );
+};
+
+Inicio.propTypes = {
+  history: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default Inicio;
