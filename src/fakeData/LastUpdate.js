046a71544aa495ca0248253b436df66928bd3e0b
Sebastian Linares <slinaresl@gmail.com>
2019-11-02 21:49:58 -0300

    fix: get days in months not number in week

--- src/fakeData/LastUpdate.js
+++ src/fakeData/LastUpdate.js
@@ -1,10 +1,6 @@
 const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1;
-const day = new Date().getDay();
+const day = new Date().getDate();
 const lastUpdate = `${day} - ${month} - ${year}`;
-// const lastUpdate = {
-//   year,
-//   month,
-//   day,
-// };
+
 export default lastUpdate;
